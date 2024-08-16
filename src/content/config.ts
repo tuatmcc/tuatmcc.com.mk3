2e8d5d0deaeb88dc1df0b6a34c55d0fc88d29c3f
ojii3 <84656786+OJII3@users.noreply.github.com>
2024-08-16 22:32:40 +0900

    [wip] add animated webp

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,12 +4,26 @@ const blog = defineCollection({
 	type: "content",
 	schema: z.object({
 		title: z.string(),
-		description: z.string(),
 		date: z.coerce.date(),
 		lastmod: z.coerce.date().optional(),
+		author: z.string().optional(),
+		description: z.string().optional(),
 		tags: z.array(z.string()).optional(),
 		image: z.string().optional(),
 	}),
 });
 
-export const collections = { blog };
+const news = defineCollection({
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		date: z.coerce.date(),
+		lastmod: z.coerce.date().optional(),
+		author: z.string().optional(),
+		description: z.string().optional(),
+		tags: z.array(z.string()).optional(),
+		image: z.string().optional(),
+	}),
+});
+
+export const collections = { blog, news };
