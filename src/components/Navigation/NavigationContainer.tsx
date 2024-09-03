cc5793ade6e0c72cb33fbe250565b19d347fda2b
ojii3 <84656786+OJII3@users.noreply.github.com>
2024-09-04 00:00:04 +0900

    feat update nav

--- src/components/Navigation/NavigationContainer.tsx
+++ src/components/Navigation/NavigationContainer.tsx
@@ -53,42 +53,63 @@ export const NavigationContainer = ({ children }: { children: ReactNode }) => {
 						/>
 					</svg>
 				</button>
-				<AnimatePresence>
-					{active && (
-						<motion.div
-							initial={{ x: "100%" }}
-							animate={{ x: 0 }}
-							exit={{ x: "100%" }}
-							transition={{ type: "spring", bounce: 0.1, duration: 0.3 }}
-							className="fixed grid top-0 bottom-0 right-0 w-[500px] max-w-[90vw]"
-							style={{
-								clipPath:
-									"polygon(0 0, 0 100px, 60px 160px, 60px calc(100% - 160px), 0 calc(100% - 100px), 0 100%, 100% 100%, 100% 0)",
-							}}
+				<motion.div
+					initial={{ x: "100%" }}
+					animate={{ x: active ? 0 : "100%" }}
+					transition={{ type: "spring", bounce: 0.1, duration: 0.3 }}
+					className="fixed grid top-0 bottom-0 right-0 w-[500px] max-w-[95vw]"
+					style={{
+						clipPath:
+							"polygon(0 0, 0 30%, 60px calc(30% + 60px), 60px calc(100% - 160px), 0 calc(100% - 100px), 0 100%, 100% 100%, 100% 0)",
+					}}
+				>
+					<div className="relative pl-16 flex flex-col bg-gray-900 p-2 overflow-y-hidden">
+						<img
+							src="/assets/technology-vertical.svg"
+							alt="Logo"
+							className="absolute opacity-10 py-4 h-full pointer-events-none"
+						/>
+
+						<button
+							type="button"
+							className="fixed z-10 top-0 right-0 p-2"
+							aria-label="Menu Button"
+							onClick={() => setActive((prev) => !prev)}
 						>
-							<div className="relative pl-16 flex flex-col bg-gray-500 p-2 overflow-y-hidden">
-								<div className="bottom-0 right-0 absolute h-full">
-									<img
-										src="/logos/logo-vertical.svg"
-										alt="Logo"
-										className="opacity-40 py-4 pt-16 h-full pointer-events-none"
-									/>
-								</div>
+							<img src="/icons/hamburger.svg" alt="Close" />
+						</button>
 
-								<button
-									type="button"
-									className="fixed z-10 top-0 right-0 p-2"
-									aria-label="Menu Button"
-									onClick={() => setActive((prev) => !prev)}
-								>
-									<img src="/icons/hamburger.svg" alt="Close" />
-								</button>
-
-								{children}
-							</div>
-						</motion.div>
-					)}
-				</AnimatePresence>
+						{children}
+					</div>
+					<div
+						className="fixed top-0 h-[30%] w-[60px] bg-red-600"
+						style={{
+							clipPath:
+								"polygon(20px 0, 22px 0, 22px 20%, 60px calc(20% + 38px), 60px calc(20% + 50px), 20px calc(20% + 12px))",
+						}}
+					/>
+					<div
+						className="fixed top-0 h-[30%] w-[60px] bg-red-600"
+						style={{
+							clipPath:
+								"polygon(20px calc(20% + 22px), 60px calc(20% + 62px), 60px calc(20% + 74px), 20px calc(20% + 36px))",
+						}}
+					/>
+					<div
+						className="fixed top-0 h-[30%] w-[60px] bg-red-600"
+						style={{
+							clipPath:
+								"polygon(20px calc(20% + 46px), 60px calc(20% + 86px), 60px calc(20% + 98px), 20px calc(20% + 60px))",
+						}}
+					/>
+					<div
+						className="fixed top-0 h-[30%] w-[60px] bg-red-600"
+						style={{
+							clipPath:
+								"polygon(58px calc(20% + 86px), 60px calc(20% + 86px), 60px calc(20% + 160px), 58px calc(20% + 160px))",
+						}}
+					/>
+				</motion.div>
 			</header>
 		</>
 	);
