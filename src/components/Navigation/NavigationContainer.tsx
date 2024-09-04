c98b7ecf5f979d0c4955a412a8b7327c28c92eef
ojii3 <84656786+OJII3@users.noreply.github.com>
2024-09-05 01:08:33 +0900

    feat update

--- src/components/Navigation/NavigationContainer.tsx
+++ src/components/Navigation/NavigationContainer.tsx
@@ -3,6 +3,7 @@ import { type ReactNode, useState } from "react";
 
 export const NavigationContainer = ({ children }: { children: ReactNode }) => {
 	const [active, setActive] = useState(false);
+
 	return (
 		<>
 			<AnimatePresence>
@@ -11,7 +12,7 @@ export const NavigationContainer = ({ children }: { children: ReactNode }) => {
 						initial={{ opacity: 0 }}
 						animate={{ opacity: 1 }}
 						exit={{ opacity: 0 }}
-						className="fixed top-0 left-0 w-full h-dvh bg-black bg-opacity-30 backdrop-blur-sm"
+						className="fixed top-0 left-0 w-full h-dvh bg-black bg-opacity-40 backdrop-blur-sm z-10"
 						onClick={() => setActive(false)}
 						onKeyDown={(e) => e.key === "Escape" && setActive(false)}
 					/>
@@ -24,40 +25,13 @@ export const NavigationContainer = ({ children }: { children: ReactNode }) => {
 					aria-label="Menu Button"
 					onClick={() => setActive((prev) => !prev)}
 				>
-					<svg
-						className=""
-						width="40"
-						height="40"
-						viewBox="0 0 24 24"
-						fill="none"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<title>Menu</title>
-						<path
-							d="M4 6L20 6"
-							stroke="black"
-							stroke-width="2"
-							stroke-linecap="round"
-						/>
-						<path
-							d="M4 12L20 12"
-							stroke="black"
-							stroke-width="2"
-							stroke-linecap="round"
-						/>
-						<path
-							d="M4 18L20 18"
-							stroke="black"
-							stroke-width="2"
-							stroke-linecap="round"
-						/>
-					</svg>
+					<img src="/icons/hamburger-black.svg" alt="Menu" />
 				</button>
 				<motion.div
 					initial={{ x: "100%" }}
 					animate={{ x: active ? 0 : "100%" }}
 					transition={{ type: "spring", bounce: 0.1, duration: 0.3 }}
-					className="fixed grid top-0 bottom-0 right-0 w-[500px] max-w-[95vw]"
+					className="fixed grid top-0 bottom-0 right-0 w-[500px] max-w-[95vw] z-10"
 					style={{
 						clipPath:
 							"polygon(0 0, 0 30%, 60px calc(30% + 60px), 60px calc(100% - 160px), 0 calc(100% - 100px), 0 100%, 100% 100%, 100% 0)",
@@ -76,34 +50,34 @@ export const NavigationContainer = ({ children }: { children: ReactNode }) => {
 							aria-label="Menu Button"
 							onClick={() => setActive((prev) => !prev)}
 						>
-							<img src="/icons/hamburger.svg" alt="Close" />
+							<img src="/icons/hamburger-white.svg" alt="Close" />
 						</button>
 
 						{children}
 					</div>
 					<div
-						className="fixed top-0 h-[30%] w-[60px] bg-red-600"
+						className="fixed top-0 h-[30%] w-[60px] bg-blue-600"
 						style={{
 							clipPath:
 								"polygon(20px 0, 22px 0, 22px 20%, 60px calc(20% + 38px), 60px calc(20% + 50px), 20px calc(20% + 12px))",
 						}}
 					/>
 					<div
-						className="fixed top-0 h-[30%] w-[60px] bg-red-600"
+						className="fixed top-0 h-[30%] w-[60px] bg-blue-600"
 						style={{
 							clipPath:
 								"polygon(20px calc(20% + 22px), 60px calc(20% + 62px), 60px calc(20% + 74px), 20px calc(20% + 36px))",
 						}}
 					/>
 					<div
-						className="fixed top-0 h-[30%] w-[60px] bg-red-600"
+						className="fixed top-0 h-[30%] w-[60px] bg-blue-600"
 						style={{
 							clipPath:
 								"polygon(20px calc(20% + 46px), 60px calc(20% + 86px), 60px calc(20% + 98px), 20px calc(20% + 60px))",
 						}}
 					/>
 					<div
-						className="fixed top-0 h-[30%] w-[60px] bg-red-600"
+						className="fixed top-0 h-[30%] w-[60px] bg-blue-600"
 						style={{
 							clipPath:
 								"polygon(58px calc(20% + 86px), 60px calc(20% + 86px), 60px calc(20% + 160px), 58px calc(20% + 160px))",
